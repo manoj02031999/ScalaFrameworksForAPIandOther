1eef78d3640f3406beeed2bd323793382a31fe0b
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-10-09 06:36:21 +0530

    API tESTING

---
+++ src/test/scala/simulations/apiTest/GetApiTest.scala
@@ -0,0 +1,21 @@
+package simulations.apiTest
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+class GetApiTest extends Simulation {
+  // PROTOCOL
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl("https://reqres.in/")
+  // SCENARIO
+  val scn: ScenarioBuilder = scenario("Get Single user Scenario")
+    .exec(
+      http("GET API for fetching sngle user data")
+        .get("api/users/2")
+    )
+
+  // SETUP
+  setUp(scn.inject(atOnceUsers(10)))
+    .protocols(httpProtocol)
+}
---
+++ src/test/scala/simulations/apiTest/PostApiTest.scala
@@ -0,0 +1,23 @@
+package simulations.apiTest
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+class PostApiTest extends Simulation{
+  // PROTOCOL
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl("https://reqres.in/")
+  // SCENARIO
+   val scn: ScenarioBuilder = scenario("Create User scenario")
+    .exec(
+      http("Post Api to Create api")
+        .post("/api/users")
+        .body(StringBody("""{
+                           |    "name": "morpheus",
+                           |    "job": "leader"
+                           |}""".stripMargin)).asJson
+    )
+ setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
+}
