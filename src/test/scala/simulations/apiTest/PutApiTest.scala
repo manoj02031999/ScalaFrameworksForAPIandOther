9f86ef8d874bc1b797f82f95c58bd949d2623c3b
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-10-09 07:39:38 +0530

    PUT and DELETE API

---
+++ src/test/scala/simulations/apiTest/PutApiTest.scala
@@ -0,0 +1,26 @@
+package simulations.apiTest
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+
+class PutApiTest extends Simulation {
+  // PROTOCOL
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl("https://reqres.in/")
+  // SCENARIO
+  val scn: ScenarioBuilder = scenario("Update User Scenario")
+    .exec(
+      http("Put api update user")
+        .put("/api/users/2")
+        .body(StringBody(
+          """{
+            |    "name": "morpheus",
+            |    "job": "zion resident"
+            |}""".stripMargin)).asJson
+    )
+
+  setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
+}
