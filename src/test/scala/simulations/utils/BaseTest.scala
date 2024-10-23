c936273ec8a63c63aafa1bf29ca8059433fa5bf7
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-10-23 11:30:02 +0530

    Removed code redundency in Base Test Class

--- src/test/scala/simulations/utils/BaseTest.scala
+++ src/test/scala/simulations/utils/BaseTest.scala
@@ -3,71 +3,45 @@ package simulations.utils
 import io.gatling.core.Predef.*
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef.*
+import io.gatling.http.request.builder.HttpRequestBuilder
 trait BaseTest {
   def getSingleUserdata(userId: Int, statusCode: Int, needRepeating: Boolean = false, repeaterCount: Int = 0): ChainBuilder = {
-    val request = exec(
-      http("GET API for fetching single user data")
+    val request = http("GET API for fetching single user data")
         .get(s"api/users/$userId")
         .check(status.is(statusCode))
-    )
-
-    if(needRepeating){
-      repeat(repeaterCount){
-        exec(request)
-      }
-    }else{
-      exec(request)
-    }
+    buildRequest(request,needRepeating, repeaterCount)
   }
 
   def createUser(requestBodyFileName: String, statusCode: Int, needRepeating: Boolean = false, repeaterCount: Int = 0): ChainBuilder = {
-    val request = exec(
-      http("Post Api to Create api")
+    val request = http("Post Api to Create api")
         .post("/api/users")
         .body(RawFileBody(requestBodyFileName)).asJson
         .check(status.is(statusCode))
-    )
-
-    if (needRepeating) {
-      repeat(repeaterCount) {
-        exec(request)
-      }
-    } else {
-      exec(request)
-    }
+    buildRequest(request,needRepeating, repeaterCount)
   }
 
   def updateUser(userId: Int, requestBodyFileName: String, statusCode: Int, needRepeating: Boolean = false, repeaterCount: Int = 0): ChainBuilder = {
-    val request = exec(
-      http("Put api update user")
+    val request = http("Put api update user")
         .put(s"/api/users/$userId")
         .body(RawFileBody(requestBodyFileName)).asJson
         .check(status.is(statusCode))
-    )
-
-    if (needRepeating) {
-      repeat(repeaterCount) {
-        exec(request)
-      }
-    } else {
-      exec(request)
-    }
+        buildRequest(request, needRepeating, repeaterCount)
   }
 
   def deleteUser(userId: Int, statusCode: Int, needRepeating: Boolean = false, repeaterCount: Int = 0): ChainBuilder = {
-    val request = exec(
-      http("Delete Api Delete user")
+    val request = http("Delete Api Delete user")
         .delete(s"/api/users/$userId")
         .check(status.is(statusCode))
-    )
+    buildRequest(request,needRepeating, repeaterCount)
+  }
 
+  private  def buildRequest(request: HttpRequestBuilder,needRepeating: Boolean = false, repeaterCount: Int = 0): ChainBuilder={
     if (needRepeating) {
-      repeat(repeaterCount) {
+      repeat(repeaterCount,"repeaterIndex") {
         exec(request)
       }
     } else {
       exec(request)
     }
   }
-
 }
