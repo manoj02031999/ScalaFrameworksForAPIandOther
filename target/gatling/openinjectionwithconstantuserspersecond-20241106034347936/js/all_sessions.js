0c7e5860898924ddff35900bf23c524cdcba1262
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-11-06 11:52:13 +0530

    Closed Model Injection and it's types like constantConcurrentUsers and rampConcurrentUsers

---
+++ target/gatling/openinjectionwithconstantuserspersecond-20241106034347936/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1730864628000,10],[1730864629000,11],[1730864630000,10],[1730864631000,11],[1730864632000,10]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
---
+++ target/gatling/openinjectionwithconstantuserspersecond-20241106034540798/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1730864742000,10],[1730864743000,12],[1730864744000,12],[1730864745000,5],[1730864746000,10],[1730864747000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
